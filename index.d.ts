03d69899c6d7ee6341d90c5cbfd99d4b78300c13
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-03 05:12:47 +0000

    Refactor TypeScript definition to CommonJS compatible export (#8)

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,41 @@
-export interface Options {
-	/**
-	 * @default 80
-	 */
-	port?: number;
+declare namespace waitForLocalhost {
+	export interface Options {
+		/**
+		@default 80
+		 */
+		port?: number;
 
-	/**
-	 * Use the `GET` HTTP-method instead of `HEAD` to check if the server is running.
-	 *
-	 * @default false
-	 */
-	useGet?: boolean;
+		/**
+		Use the `GET` HTTP-method instead of `HEAD` to check if the server is running.
+
+		@default false
+		 */
+		useGet?: boolean;
+	}
 }
 
-/**
- * Wait for localhost to be ready.
- */
-export default function waitForLocalhost(options?: Options): Promise<void>;
+declare const waitForLocalhost: {
+	/**
+	Wait for localhost to be ready.
+
+	@example
+	```
+	import waitForLocalhost = require('wait-for-localhost');
+
+	(async () => {
+		await waitForLocalhost({port: 8080});
+		console.log('Server is ready');
+	})();
+	```
+	 */
+	(options?: waitForLocalhost.Options): Promise<void>;
+
+	// TODO: Remove this for the next major release, refactor the whole definition to:
+	// declare function waitForLocalhost(
+	// 	options?: waitForLocalhost.Options
+	// ): Promise<void>;
+	// export = waitForLocalhost;
+	default: typeof waitForLocalhost;
+};
+
+export = waitForLocalhost;
